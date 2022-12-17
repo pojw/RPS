359fbfc151a007e847a91ee86087c66bbb32b393
pojw <jaylinhernandez123@icloud.com>
2022-12-17 12:13:20 -0500

    i got it all done, the behind the scences done but not the best looking one and could need more features

--- java.js
+++ java.js
@@ -1,32 +1,62 @@
 function Hey(choice){
-    console.log(choice)
-    const response= document.querySelector("#response")
-    response.removeChild(response.lastChild)
+    let scoreWins = document.querySelector(".winNumber")
+    let scoreLost= document.querySelector(".lostNumber")
+    let result = document.querySelector("#answer")
+    
 
-    const texts=document.createElement('div')
-    texts.classList.add("content")
-
-    texts.textContent=`${choice}`
-
-    response.appendChild(texts)
-
-
-
-
+   
+    
     let list= ["paper","rock","scisscors"]
     let computerChoice= list[Math.floor(Math.random()*3)]
-    console.log(computerChoice)
+
     if(choice===computerChoice){
-        console.log(`${choice} ties  ${computerChoice}`)
+       result.textContent="tie good job"
+   
     }
-    else if(choice==="paper"&& computerChoice==="rock"){
-        console.log(`${choice} beats  ${computerChoice}`)
-    }
-    else if (choice === "rock "&& computerChoice==="scisscors"){
-        console.log (`${choice} beats  ${computerChoice}`)
-    }
-    else if (choice==="scisscors"&& computerChoice==="paper "){
-        console.log(`${choice} beats  ${computerChoice}`)
-    }
-    else{console.log (`${choice} loses to ${computerChoice}`)}
+     else if(choice === "rock"&& computerChoice==="scisscors"){
+       result.textContent=`${choice } beats ${computerChoice}`
+       scoreWins.textContent+="I"    }
+
+    else if (choice==="paper"&& computerChoice==="rock"){
+    result.textContent=`${choice } beats ${computerChoice}`
+    scoreWins.textContent+="I"
+   }
+    else if(choice==="scisscors"&& computerChoice==="paper"){
+        result.textContent=`${choice } beats ${computerChoice}`
+        scoreWins.textContent+="I"    }
+    
+    else{  
+        scoreLost.textContent+="I"
+        result.textContent=`${choice} loses to ${computerChoice}` 
+
 }
+console.log( choice )
+console.log( computerChoice )
+
+}
+
+function startGame(){
+    const wins=document.querySelector("#wins")
+
+    if( wins.children.length > 0){
+        const zeroWinNumber=document.querySelector(".winNumber")
+        zeroWinNumber.textContent="I"  
+        const  zeroLostNumber = document.querySelector(".lostNumber")
+        zeroLostNumber.textContent="I"
+    }
+    else{
+    const wins=document.querySelector("#wins")
+
+    const winNumber = document.createElement("div")
+    winNumber.classList.add("winNumber")
+    winNumber.textContent="I"
+
+    wins.appendChild(winNumber)
+
+    const lost = document.querySelector("#lost")
+
+    const lostNumber = document.createElement("div")
+    lostNumber.classList.add("lostNumber")
+    lostNumber.textContent="I"
+    lost.appendChild(lostNumber)
+    }}
